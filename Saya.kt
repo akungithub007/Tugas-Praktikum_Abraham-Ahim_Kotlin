6a533919fb7da9a8633dafc566e9aa7ab8e0e5ae
akungithub007 <96878297+akungithub007@users.noreply.github.com>
2021-12-31 19:14:08 +0800

    Add files via upload

---
+++ Saya.kt
@@ -0,0 +1,16 @@
+package TugasPraktikum
+
+class Saya {
+
+    var NamaPanjang: String = ""
+    var NamaPendek: String = ""
+
+    fun katakanHalo(nama: String) {
+        println("Halo $nama, Nama saya adalah $NamaPanjang")
+    }
+
+}
+
+//Jadi Method/Function yang saya buat,saya masukkan kedalam class(Saya) yg sudah saya buat.
+//jadi function diatas yaitu katakanHalo,dengan parameter nama,kemudian tipe datanya String.
+//Kemudian menggunakan println yaitu perintah untuk menampilkan output Halo $nama(Nico), Nama saya adalah $Abraham Ahim W
