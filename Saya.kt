0463e382516f85d421423bc4c9d71ce9a70e8e9b
akungithub007 <96878297+akungithub007@users.noreply.github.com>
2021-12-31 19:18:02 +0800

    Update Saya.kt

--- Saya.kt
+++ Saya.kt
@@ -13,4 +13,4 @@ class Saya {
 
 //Jadi Method/Function yang saya buat,saya masukkan kedalam class(Saya) yg sudah saya buat.
 //jadi function diatas yaitu katakanHalo,dengan parameter nama,kemudian tipe datanya String.
-//Kemudian menggunakan println yaitu perintah untuk menampilkan output Halo $nama(Nico), Nama saya adalah $Abraham Ahim W
+//Kemudian menggunakan println yaitu perintah untuk menampilkan output Halo $nama(Nico), Nama saya adalah $NamaPanjang(Abraham Ahim W).
