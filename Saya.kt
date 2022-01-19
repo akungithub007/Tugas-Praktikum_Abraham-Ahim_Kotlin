5593a750fd1827d31e6af1fd1b4a54ced4fd2058
akungithub007 <96878297+akungithub007@users.noreply.github.com>
2022-01-19 20:32:30 +0800

    Add files via upload

--- Saya.kt
+++ Saya.kt
@@ -11,6 +11,17 @@ class Saya {
 
 }
 
+open class Ucapan {
+    fun pesan () {
+        print("Selamat Belajar Baim")
+    }
+}
+class Selamat: Ucapan(){
+}
+fun main() {
+    var  view = Selamat()
+    view.pesan()
+}
 //Jadi Method/Function yang saya buat,saya masukkan kedalam class(Saya) yg sudah saya buat.
 //jadi function diatas yaitu katakanHalo,dengan parameter nama,kemudian tipe datanya String.
 //Kemudian menggunakan println yaitu perintah untuk menampilkan output Halo $nama(Nico), Nama saya adalah $NamaPanjang(Abraham Ahim W).
