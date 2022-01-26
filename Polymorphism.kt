e4d483444affb386f10ecc2d32339f2b291e3ec3
akungithub007 <96878297+akungithub007@users.noreply.github.com>
2022-01-26 21:51:05 +0800

    Add files via upload

--- Polymorphism.kt
+++ Polymorphism.kt
@@ -11,6 +11,9 @@ fun main() {
     mahasiswa.sayHello("Asran")
 }
 
-
+//Polymorphism yaitu kemampuan untuk merubah bentuk.
+//Jadi disini saya merubah dari Mahasiswa menjadi Ketuakelas,begitupun seterusnya.
+//Maka ketika kita running,maka hasilnya akan berubah bentuk dari seebelumnya.
+//Walaupun mahasiswa.sayHello nya tetap sama,tapi karena dia berubah bentuk,maka behaviornya juga akan berubah.
 
 
