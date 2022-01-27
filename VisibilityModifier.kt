4de15edfc5f88714e15f9221370120da62c030c0
akungithub007 <96878297+akungithub007@users.noreply.github.com>
2022-01-27 22:13:56 +0800

    Update VisibilityModifier.kt

--- VisibilityModifier.kt
+++ VisibilityModifier.kt
@@ -1,5 +1,14 @@
 package TugasPraktikum
 
+class Guru (val name:String) {
+    fun guru(){
+        println("Guru!")
+    }
+}
+
+
+package TugasPraktikum
+
 fun main() {
     val guru = Guru("Nama Gurunya adalah Munawir")
     println(guru.name)
