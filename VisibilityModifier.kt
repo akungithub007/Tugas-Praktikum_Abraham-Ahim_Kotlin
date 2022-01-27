542481ed68d73c64d10933723910e7055dfd7f03
akungithub007 <96878297+akungithub007@users.noreply.github.com>
2022-01-27 22:13:22 +0800

    Add files via upload

---
+++ VisibilityModifier.kt
@@ -0,0 +1,9 @@
+package TugasPraktikum
+
+fun main() {
+    val guru = Guru("Nama Gurunya adalah Munawir")
+    println(guru.name)
+}
+
+//Ada 4 visibility modifier dikotlin yaitu: Publik, Private, Protected, dan yang terakhir itu Internal.
+//visibility modifier yang saya gunakan diatas yaitu bersifat publik,yang artinya bisa diakses dari mana saja
